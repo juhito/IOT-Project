4c0f2f2dba6ddc421965016a388f473ff4c992fc
Jussi Perälä <Jussi8@hotmail.com>
2021-12-12 01:37:59 +0200

    receive&send messages through firebase

--- src/fcm.js
+++ src/fcm.js
@@ -4,21 +4,47 @@ import fs from "fs";
 
 /** Class representing communication with Google Firebase */
 class FCM {
+    #fb_config;
+    #db;
+    
     constructor() {
         this.#fb_config = JSON.parse(fs.readFileSync(process.env.FCM_CONFIG_PATH));
+
         fb.initializeApp({
             credential: fb.credential.cert(this.#fb_config),
             databaseURL: process.env.FCM_DATABASE_URL
         });
 
-        this.#db = fb.database();
+        this.#db = fb.database(fb);
     }
 
-    testConnection() {
-        const ref = db.ref("Tokens");
-        ref.once("value", (snapshop) => {
-            console.log(snapshop.val());
-        })
+    listenForPauseRequests(sensor) {
+        const requests = this.#db.ref().child("pauseRequests");
+        requests.on("child_added", (requestSnapshot) => {
+            const request = requestSnapshot.val();
+            let obj = {};
+
+            sensor.togglePause();
+
+            if(sensor.pauseSensor) {
+                obj.title = "Pausing request received.";
+                obj.body = "Pausing sensor now."; 
+            }
+            else {
+                obj.title = "Unpause request received.";
+                obj.body = "Unpausing sensor now.";
+            }
+
+            getMessaging().send({
+                notification: obj,
+                token: request.token
+            }).then((data) => {
+                console.log("Sending notification back to user, removing from db!");
+                requestSnapshot.ref.remove();
+            }).catch((error) => {
+                console.log(error);
+            });
+        });
     }
 };
 
